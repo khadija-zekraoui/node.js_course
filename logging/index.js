db4f3c25b4a07f427cc4769110bdd4fb2c86f115
DoujaDev <h.zekraoui.khadija@gmail.com>
2020-06-25 14:50:21 +0200

    (RegEx) Tutorial #1 - What is RegEx?

--- logging/index.js
+++ logging/index.js
@@ -3,9 +3,27 @@ const express = require("express");
 const pino = require("pino");
 const expressPino = require("express-pino-logger");
 
-const logger = pino({
-  level: process.env.LOG_LEVEL,
-  prettyPrint: true,
+import * as pino from "pino";
+const chalk = require("chalk");
+export const log = pino({
+  level: process.env.LOGLEVEL,
+  base: null,
+  /*formatters: {  
+        level: (label) => {
+          return { level: label };
+        },
+    },*/
+  serializers: {
+    req: pino.stdSerializers.req,
+    res: pino.stdSerializers.res,
+    err: pino.stdSerializers.err,
+  },
+  //timestamp:pino.stdTimeFunctions.isoTime,
+  prettyPrint: {
+    colorize: true,
+    errorLikeObjectKeys: ["err", "error"],
+    translateTime: "dd-mm-yyyy hh:mm:ss.Ms", // human legible date
+  },
 });
 const expressLogger = expressPino({ logger });
 
